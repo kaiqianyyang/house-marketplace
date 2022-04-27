b6055104bbcf0e78522f86e8eae69205049bd061
Kaiqian Yang <kaiqianyang@Kaiqians-MacBook-Pro-2.local>
2022-04-27 05:35:03 -0700

    Working with Swiper v8.0.7

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,13 +1,26 @@
 // rfce+tab
 import { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
+// import { Helmet } from 'react-helmet'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+// import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+// import { Swiper, SwiperSlide } from 'swiper/react'
+// import 'swiper/swiper-bundle.css'
 import { getDoc, doc } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { db } from '../firebase.config';
 import Spinner from '../components/Spinner';
 import shareIcon from '../assets/svg/shareIcon.svg';
-import { async } from '@firebase/util';
+// SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
+
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/a11y';
 
 function Listing() {
   const [listing, setListing] = useState(null);
@@ -39,6 +52,31 @@ function Listing() {
   return (
     <main>
       {/* SLIDER */}
+      {/* <Helmet> */}
+      <title>{listing.name}</title>
+      {/* </Helmet> */}
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        navigation
+        style={{ height: '300px' }}
+      >
+        {listing.imgUrls.map((url, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <div
+                className="swiperSlideDiv"
+                style={{
+                  background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                  backgroundSize: 'cover',
+                }}
+              ></div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+
       <div
         className="shareIconDiv"
         onClick={() => {
@@ -109,13 +147,10 @@ function Listing() {
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
             />
-            
-            <Marker
-              position={[listing.latitude, listing.longitude]}
-            >
+
+            <Marker position={[listing.latitude, listing.longitude]}>
               <Popup>{listing.location}</Popup>
             </Marker>
-
           </MapContainer>
         </div>
 
